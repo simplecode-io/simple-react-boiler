4929f588d3104e6afd2a6e32808a6c2e5d9c5686
Owen Phillips <owen@cloudgiant.ca>
2021-01-06 10:13:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Simple React Boiler
+
+Essential react boiler with
+
+- Ready for production and development
+- Minimial React boiler
+
+## Babel
+
+## Webpack
+
+Includes individual build scripts for produciton and development
+
+-Automatically wipe and rebuild the public directory
+-Analyze addon
+
+### Development
+
+-Source maps
+-Dev server with hot reloading
+
+## Version Control
+
+-Includes minimal .gitignore
+
+## Jest
+
+## SASS
+
+## Usage
+
+Run `npm run develop` to run the webpack development server
+Run `npm run build` to have webpack build your production bundle in the public folder
---
+++ build-utils/webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "React Boiler",
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "..", "./public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "..", "./public"),
+    hot: true,
+  },
+};
